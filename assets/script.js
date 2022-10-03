d61446e7668976341c75fb5ebd86998ef0518d37
Richie Thiesfeldt <richiethie@gmail.com>
2022-10-02 21:32:15 -0500

    Started trying to get textarea to display correct color

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
 var currentDay = $('#currentDay')
-var timeOfDay = $('#timeOfDay')
-var textArea = $('#textArea')
+var timeOfDay = $('.hour')
+var textArea = $('.form-control')
 var saveBtn = $('button')
+var timeSection = $('.input-group')
 
 currentDay.innerText = moment().format('dddd, MMM Do YYYY, h:mm a')
-console.log(parseInt('8AM'))
-console.log(moment().hour())
+
+
 function saveNote() {
-    
+    var inputValue = textArea.val()
+    console.log(inputValue)
     // when button is clicked, save text to local storage
     // get items from local storage
 
@@ -15,8 +17,27 @@ function saveNote() {
 
 function currentHour() {
     // if statement to display color on < currenthour, = currenthour, > currenthour
-    var hourNow = moment().hour()
-    // if ()
+    timeSection.each(function() {
+        var hourLoop = parseInt($(this).attr('id')[1])
+        var hourNow =  18 //moment().hour()
+        console.log(hourLoop)
+        console.log(hourNow)
+        if (hourNow > hourLoop) {
+            $(textArea).removeClass('present')
+            $(textArea).removeClass('future')
+            $(textArea).addClass('past')
+        } else if (hourNow === hourLoop) {
+            $(textArea).removeClass('past')
+            $(textArea).removeClass('future')
+            $(textArea).addClass('present')
+        } else {
+            $(textArea).removeClass('past')
+            $(textArea).removeClass('present')
+            $(textArea).addClass('future')
+        }
+    })
+    
 }
-
-saveBtn.on('submit', saveNote)
+saveNote()
+currentHour()
+saveBtn.on('click', saveNote)
